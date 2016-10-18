5ed0914d70a23aea999eafaaa2dae365923f9ebc
Cristian Jimenez <38853841x@iespoblenou.org>
2016-10-18 18:29:29 +0200

    Creem un ArrayAdapter<String> buit i el connectem al ListView amb un adapter.

--- app/src/main/java/com/example/a38853841x/listadomagic/MainActivityFragment.java
+++ app/src/main/java/com/example/a38853841x/listadomagic/MainActivityFragment.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -20,6 +26,17 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ListView lvCards = (ListView) view.findViewById(R.id.lvCards);
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lv_cards_row,
+                R.id.tvCard,
+                items
+                );
+
+        lvCards.setAdapter(adapter);
+
         return view;
     }
 }
