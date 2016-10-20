3708f2b3e5ff8009c4939f8fda77f7d1c5ec495a
Cristian Jimenez <38853841x@iespoblenou.org>
2016-10-21 00:02:44 +0200

    Actualitzem l'adapter amb les dades del JSON

--- app/src/main/java/com/example/a38853841x/listadomagic/MainActivityFragment.java
+++ app/src/main/java/com/example/a38853841x/listadomagic/MainActivityFragment.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
