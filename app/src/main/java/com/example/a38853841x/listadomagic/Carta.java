822230103a70992b0cb8f92d07cd8a8a94e66f4a
Cristian Jimenez <38853841x@iespoblenou.org>
2016-10-20 21:43:52 +0200

    Creem el pojo que guardara l'informació que baixem d'internet i a agreguem la classe HttpUtils, que utilitzarem per a fer peticions HTTP

---
+++ app/src/main/java/com/example/a38853841x/listadomagic/Carta.java
@@ -0,0 +1,50 @@
+package com.example.a38853841x.listadomagic;
+
+import android.support.design.widget.Snackbar;
+
+import java.security.PrivateKey;
+
+/**
+ * Created by Kamelot on 20/10/2016.
+ */
+
+public class Carta {
+
+    private String title;
+    private String type;
+    private String imageUrl;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Carta{" +
+                "title='" + title + '\'' +
+                ", type='" + type + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
