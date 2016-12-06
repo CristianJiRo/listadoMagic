7b2f43b5d6470201fd7fdc1a9716847ef0365306
Cristian Jimenez <38853841x@iespoblenou.org>
2016-12-06 19:49:27 +0100

    Baixem varies pàgines de dades.

--- app/src/main/java/com/example/a38853841x/listadomagic/MagicApi.java
+++ app/src/main/java/com/example/a38853841x/listadomagic/MagicApi.java
@@ -1,6 +1,7 @@
 package com.example.a38853841x.listadomagic;
 
 import android.net.Uri;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,15 +17,89 @@ import java.util.ArrayList;
 
 class MagicApi {
     static final String Base_URL = "https://api.magicthegathering.io/v1/cards";
+    static final int pages =10;
 
-    static ArrayList<Carta> getCartes() {
+    private static String getUrlPage(int page){
+        String url;
         Uri builtUri = Uri.parse(Base_URL)
-            .buildUpon()
-            .build();
-        String url = builtUri.toString();
-        return doCall(url);
+                .buildUpon()
+                .appendQueryParameter("page", String.valueOf(page))
+                .build();
+        url = builtUri.toString();
+
+        return url;
     }
 
+    static ArrayList<Carta> getCartes() {
+
+        return doCall();
+    }
+
+    static ArrayList<Carta> doCall() {
+
+        ArrayList<Carta> cards = new ArrayList<>();
+
+        for (int i = 0; i < pages ; i++) {
+
+            try {
+                String JsonResponse = HttpUtils.get(getUrlPage(i+1));
+                ArrayList<Carta> list = processJson(JsonResponse);
+                cards.addAll(list);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return cards;
+
+    }
+
+    static ArrayList<Carta> processJson(String jsonResponse) {
+
+        ArrayList<Carta> cards = new ArrayList<>();
+        try {
+            JSONObject data = new JSONObject(jsonResponse);
+            JSONArray jsonCartas = data.getJSONArray("cards");
+            for (int i = 0; i < jsonCartas.length(); i++) {
+
+                JSONObject jsonCarta = jsonCartas.getJSONObject(i);
+
+                Carta carta = new Carta();
+                carta.setTitle(jsonCarta.getString("name"));
+                carta.setType(jsonCarta.getString("type"));
+                carta.setImageUrl(jsonCarta.getString("imageUrl"));
+                carta.setRarity(jsonCarta.getString("rarity"));
+
+                if (jsonCarta.isNull("colors")){
+                    carta.setColors("Uncolor");
+                }
+                else{
+                    carta.setColors(jsonCarta.getString("colors"));
+                }
+
+                cards.add(carta);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return cards;
+    }
+
+
+
+    //Parte del fichero temporal a la espera de modificar las busquedas en la BD.
+    static ArrayList<Carta> doCallOld (String url) {
+
+        try {
+            String JsonResponse = HttpUtils.get(url);
+            return processJson(JsonResponse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
 
     static ArrayList<Carta> getCartesFilterRarity (String rarity) {
         Uri builtUri = Uri.parse(Base_URL)
@@ -32,7 +107,7 @@ class MagicApi {
                 .appendQueryParameter("rarity", rarity)
                 .build();
         String url = builtUri.toString();
-        return doCall(url);
+        return doCallOld(url);
     }
 
     static ArrayList<Carta> getCartesFilterColor (String colors) {
@@ -41,7 +116,7 @@ class MagicApi {
                 .appendQueryParameter("colors", colors)
                 .build();
         String url = builtUri.toString();
-        return doCall(url);
+        return doCallOld(url);
     }
 
     static ArrayList<Carta> getCartesFilters (String colors, String rarity) {
@@ -51,53 +126,13 @@ class MagicApi {
                 .appendQueryParameter("rarity", rarity)
                 .build();
         String url = builtUri.toString();
-        return doCall(url);
+        return doCallOld(url);
     }
 
 
-    static ArrayList<Carta> doCall(String url) {
-        try {
-            String JsonResponse = HttpUtils.get(url);
-            return processJson(JsonResponse);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
 
-        }
 
-    static ArrayList<Carta> processJson(String jsonResponse) {
 
-        ArrayList<Carta> cards = new ArrayList<>();
-            try {
-                JSONObject data = new JSONObject(jsonResponse);
-                JSONArray jsonCartas = data.getJSONArray("cards");
-                for (int i = 0; i < jsonCartas.length(); i++) {
-
-                    JSONObject jsonCarta = jsonCartas.getJSONObject(i);
-
-                    Carta carta = new Carta();
-                    carta.setTitle(jsonCarta.getString("name"));
-                    carta.setType(jsonCarta.getString("type"));
-                    carta.setImageUrl(jsonCarta.getString("imageUrl"));
-                    carta.setRarity(jsonCarta.getString("rarity"));
-
-                    if (jsonCarta.isNull("colors")){
-                        carta.setColors("Uncolor");
-                    }
-                    else{
-                        carta.setColors(jsonCarta.getString("colors"));
-                    }
-
-                    cards.add(carta);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-               return cards;
-    }
 }
 
 
